c1c153caca3b126a20603bc816297464574769eb
Sam Stites <stites@users.noreply.github.com>
2022-04-02 13:00:12 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "shell for redirect-to-abstract";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShell = with pkgs; mkShell {
+	    buildInputs = [ nodePackages.browserify ];
+          };
+        }
+      );
+}
