9da676635b3340dfd8c26b7ea5c525bc0294cacd
Sam Stites <stites@users.noreply.github.com>
2022-04-05 12:04:05 -0400

    @!*\!!!!

--- js/content.js
+++ js/content.js
@@ -1,60 +1,67 @@
 var sites = require('./sites.js');
 
-console.debug("[redirect-to-abstract] starting...");
+var maxcount = 12; // 9 for slow connections, 12 for very slow connections
+const header = "[redirect-to-abstract]";
+const mkctx = (ctx) => "["+ctx+"]";
+const mkerr = (msg) => "Error! "+msg+ " Please file an issue at https://github.com/stites/redirect-to-abstract";
 
-function domReady() {
-  console.debug("[redirect-to-abstract] mapping over all sites.js");
-  sites.all.map(filterPair).some((ffr)=> {
-    let filter = ffr[0];
-    let fn = ffr[1];
-    let r = ffr[2];
-    let runit = filter(window.location.hostname)
-    return runit && ( fn() || true )
-  })
-}
-
-function filterPair(redirectee) {
-  return [
-    (hn) => redirectee.domain === hn,
-    () => ensureRedirection(redirectee),
-    redirectee
-  ];
-}
-
-var count = 0;
-var maxcount = 10;
-
-function adjustPdfUrl(redirectee) {
+function adjustPdfUrl(ctx, redirectee) {
   const found = redirectee.pdfSelector();
   found.forEach((el) => {
-    const href = el.getAttribute("href")
+    const href = el.getAttribute("href");
     if (href[href.length-1] !== redirectee.breakPdfRegex) {
-      el.setAttribute("href", href+redirectee.breakPdfRegex)
-      console.debug("[redirect-to-abstract] adujsting url ", href, "->", el.getAttribute("href"))
+      el.setAttribute("href", href+redirectee.breakPdfRegex);
+      console.debug(header, mkctx(ctx), "adujsting url "+ href+ "->"+ el.getAttribute("href"));
     }
   });
-  count += 1;
-  ensureRedirection(redirectee);
 }
 
-function ensureRedirection(redirectee) {
-  const found = redirectee.pdfSelector();
-  if (found.length === 0) {
-    console.error("[redirect-to-abstract] pdf selector no longer valid! please file an issue at https://github.com/stites/redirect-to-abstract")
-  } else if (count > maxcount) {
-    console.debug("[redirect-to-abstract] no longer rewriting urls");
+function idx2schedule(i) {
+  // exponential backoff, stretched out a bit.
+  if (i === 0) {
+    return 0;
   } else {
-    const timeout = count * count;
-    console.debug("[redirect-to-abstract] checking urls again in", timeout, "seconds (", count, "/", maxcount,")")
-    setTimeout(
-      adjustPdfUrl.bind(null, redirectee),
-      1000 * timeout
-    );
+    const n = Math.exp(i)/(i*i) * 150; // this schedule looks good
+    const ms = Math.floor(n);          // convert to ms
+    return ms;
   }
 }
 
-if (window.location.hostname.includes("neurips")) {
-  domReady();
-} else {
-  document.addEventListener('DOMContentLoaded', domReady);
+function schedule(ctx, maxcount, count, redirectee) {
+  const ms = idx2schedule(count);
+  const s = ms / 1000;
+  console.debug(header, mkctx(ctx), "checking urls again in", s, "seconds ("+ count+ "/"+ maxcount+")");
+  return setTimeout(() => {
+    ensureRedirection(ctx, maxcount, count, redirectee);
+  }, ms);
 }
+
+function ensureRedirection(ctx, maxcount, count, redirectee) {
+  const found = redirectee.pdfSelector();
+  if (found.length === 0) {
+    myerror("pdf selector no longer valid!", ctx);
+  } else if (count > (maxcount-1)) { // adjust for 0 indexing
+    console.debug(header, mkctx(ctx), "no longer rewriting urls");
+  } else {
+    adjustPdfUrl(ctx, redirectee);
+    schedule(ctx, maxcount, count+1, redirectee);
+  }
+}
+
+function main(ctx, maxcount) {
+  console.debug(header, "starting schedule:", [...Array(maxcount).keys()].map((i) => idx2schedule(i) / 1000));
+  const rs = sites.all.filter((r) => r.domain === window.location.hostname);
+  if (rs.length !== 1) {
+    console.error(header, mkctx(ctx), mkerr("multiple sites found!"), rs);
+  } else {
+    // add hooks that I don't trust:
+    document.addEventListener('DOMContentLoaded', (event) => { adjustPdfUrl('document.DOMContentLoaded', rs[0]); });
+    window.addEventListener('load', (event) => {  adjustPdfUrl('window.load', rs[0]); });
+
+    // add schedule which I do trust
+    ensureRedirection(ctx, maxcount, 0, rs[0]);
+  }
+}
+
+
+main("main", maxcount);
